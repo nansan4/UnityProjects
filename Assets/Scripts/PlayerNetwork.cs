af48ad921bec23d04840164fe51ca411d351271c
nansan4 <nando@santosalliances.com>
2025-12-01 15:57:38 -0600

    Progress

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour
+{
+   
+    void Update()
+    {
+        Vector3 moveDir = new Vector3(0,0,0);
+        if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
+        if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
+        if (Input.GetKey(KeyCode.A)) moveDir.x = -1f;
+        if (Input.GetKey(KeyCode.D)) moveDir.x = +1f;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
