e589dd3abca4a209d0dbcf3d2dc419c502006aaf
nansan4 <nando@santosalliances.com>
2025-12-02 15:23:13 -0600

    added player movement ownership

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
    
     void Update()
     {
+        if(!IsOwner) return;
+
         Vector3 moveDir = new Vector3(0,0,0);
         if (Input.GetKey(KeyCode.W)) moveDir.z = +1f;
         if (Input.GetKey(KeyCode.S)) moveDir.z = -1f;
